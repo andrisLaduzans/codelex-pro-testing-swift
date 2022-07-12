522d5ae21357f0864702311416e3f20136850d00
Andris Laduzans <andris.laduzans@gmail.com>
2022-07-12 22:34:45 +0300

    Initial Commit

---
+++ UnitTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitTesting
+//
+//  Created by Andris Laduzans on 12/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnitTesting/UnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingApp.swift
+//  UnitTesting
+//
+//  Created by Andris Laduzans on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
