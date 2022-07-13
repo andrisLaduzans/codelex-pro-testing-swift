d6f88baf7a2072400e4c14945ee8b8e6b3161f40
Andris Laduzans <andris.laduzans@gmail.com>
2022-07-14 00:11:29 +0300

    add Lesson_01 exercises kata completed with tests

--- UnitTesting/Lesson01/AccountStatement.swift
+++ UnitTesting/Lesson01/AccountStatement.swift
@@ -9,17 +9,65 @@ import Foundation
 
 struct Account {
     private(set) var balance: Int = 0
+    private(set) var history: [AccountHistoryItem] = []
     
-    mutating func deposit(amount: Int) -> Void {
+    mutating func deposit(amount: Int, date: Date = Date.now) -> Void {
         print("\n\(amount)$ added to account")
         balance += amount
+        addHistory(amount: amount, transaction: .deposit, date: date)
     }
     
-    func withdraw(amount: Int) -> Void {
-        print("\(amount) was withdrawn from account")
+    mutating func withdraw(amount: Int, date: Date = Date.now) throws -> Void {
+        print("\n\(amount) was withdrawn from account")
+        if balance - amount < 0 {
+            throw AccountError.insufficientBalance(currentBalance: balance)
+        } else {
+            balance -= amount
+            addHistory(amount: amount, transaction: .withdraw, date: date)
+        }
     }
     
     func printStatement() -> String {
-        return "Your balance is \(balance)"
+        let parsedHistory: String = history.reduce("\(printTab("Date", 15))\(printTab("Amount"))\(printTab("Balance"))") { acc, curr in
+            
+            return acc + "\n\(printTab(formatDate(date:curr.date), 15))\(printTab(curr.transactionAmount))\(printTab(String(curr.balance)))"
+        }
+        
+        print("\n\(parsedHistory)")
+        return parsedHistory
     }
+    
+    private mutating func addHistory(amount: Int, transaction: Transaction, date: Date) -> Void {
+        history.append(AccountHistoryItem(date: date, transactionAmount: "\(transaction.rawValue)\(amount)", balance: balance))
+    }
+    
+    private enum Transaction: Character {
+        case deposit = "+"
+        case withdraw = "-"
+    }
+    
+    private func formatDate(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.setLocalizedDateFormatFromTemplate("dd.MM.yyyy")
+        let formattedDate = dateFormatter.string(from: date)
+        return formattedDate
+    }
+    
+    func printTab(_ text: String, _ length: Int = 10) -> String {
+        var result = text
+        for _ in 0..<(length - text.count) {
+            result += " "
+        }
+        return result
+    }
+}
+
+struct AccountHistoryItem {
+    let date: Date
+    let transactionAmount: String
+    let balance: Int
+}
+
+enum AccountError: Error, Equatable {
+    case insufficientBalance(currentBalance: Int)
 }
--- UnitTestingTests/AccountStatementTests.swift
+++ UnitTestingTests/AccountStatementTests.swift
@@ -29,4 +29,35 @@ class AccountStatementTests: XCTestCase {
         account.deposit(amount: 10)
         XCTAssertEqual(account.balance, 510, "balance should become 510 after added 10 more")
     }
+    
+    func test_can_withdraw() throws {
+        account.deposit(amount: 500)
+        try account.withdraw(amount: 100)
+        XCTAssertEqual(account.balance, 400, "balance should be 400 after withdrawing 100 from 500")
+    }
+    
+    func test_cannot_withtdraw_if_insufficient_balance() {
+        account.deposit(amount: 400)
+        let expectedError = AccountError.insufficientBalance(currentBalance: 400)
+        var error: AccountError?
+        
+        XCTAssertThrowsError(try account.withdraw(amount: 500), "withdraw method should throw if withdrawable amount exceeds current balance") { thrownError in
+            error = thrownError as? AccountError
+        }
+        
+        XCTAssertEqual(expectedError, error)
+    }
+    
+    func test_print_statement() throws {
+        account.deposit(amount: 100, date: Date(timeIntervalSince1970: 1000))
+        try account.withdraw(amount: 20, date: Date(timeIntervalSince1970: 1000))
+        let history = account.printStatement()
+        let expected = "Date           Amount    Balance   \n01/01/1970     +100      100       \n01/01/1970     -20       80        "
+        XCTAssertEqual(history, expected, "print statement should print formatted account hisotory")
+    }
+    
+    func test_print_tab() {
+        let text = "Date"
+        XCTAssertEqual(account.printTab(text, 10).count, 10, "print tab method should add whitespace to text till it reaches required length")
+    }
 }
